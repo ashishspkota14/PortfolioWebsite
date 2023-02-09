df56adcc0da3ecf256c77846908df26399efdc6e
Aashish Sapkota <ashishsapkota14@GMAIL.COM>
2023-02-09 13:38:14 -0600

    Header component Completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png";
+import ME from "../../assets/me.jpg";
 import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello i'm h5</h5>
+        <h5>Hello I am</h5>
         <h1>Aashish Sapkota</h1>
         <h5 className="text-light"> Fullstack Developer</h5>
         <CTA />
